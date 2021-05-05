5a16036bb80720a2d9092a90c678eff94d50aeae
thomaslty <thomaslty16@gmail.com>
2021-05-05 19:25:43 +0800

    add cart remove btn

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,71 +1,79 @@
-import "./ShoppingCart.css"
-import { useState } from 'react';
-import { InputNumber, Form, Button,Input } from 'antd';
-import {shoppingCartService} from "../Service";
-
+import "./ShoppingCart.css";
+import { useState } from "react";
+import { InputNumber, Form, Button, Input } from "antd";
+import { shoppingCartService } from "../Service";
+import { MinusCircleOutlined } from "@ant-design/icons";
+import { useShoppingCartDelete } from "../contexts/ShoppingCartContext";
 
 const getLocalStorageKeys = () => {
-    let res = [];
-    for (var i = 0; i < localStorage.length; i++){
-        res.push(localStorage.key(i));
-    }
-    res.sort();
-    return res;
-}
+  let res = [];
+  for (var i = 0; i < localStorage.length; i++) {
+    res.push(localStorage.key(i));
+  }
+  res.sort();
+  return res;
+};
 
 const ShoppingCart = (props) => {
-    const [updateTotalCost, setupdateTotalCost] = useState(true);
+  const [updateTotalCost, setupdateTotalCost] = useState(true);
+  //const [deleteShoppingCart, setDeleteShoppingCart] = useState(true);
+  const removeShoppingList = useShoppingCartDelete();
 
-    const onFinish = () => {
-        let order = [];
-        var keys = getLocalStorageKeys();
-        keys.forEach(key => {
-            order.push({key: key, value: localStorage.getItem(key)});
+  const onFinish = () => {
+    let order = [];
+    var keys = getLocalStorageKeys();
+    keys.forEach((key) => {
+      order.push({ key: key, value: localStorage.getItem(key) });
+    });
+    shoppingCartService(order).then((res) => {
+      console.log("got return");
+    });
+  };
+
+  const getShoppingCartList =
+    props.productList.length === 0
+      ? []
+      : getLocalStorageKeys().map((key) => {
+          return props.productList.filter((product) => {
+            return product.pid === key;
+          })[0];
         });
-        shoppingCartService(order).then(res => {
-            console.log("got return");
-        });
-    }
 
-    const getShoppingCartList = props.productList.length === 0 
-        ? [] 
-        : getLocalStorageKeys().map(key => {
-            return props.productList.filter(product => {
-                return product.pid === key;
-            })[0];
-        });
+  const shoppingCartElements = getShoppingCartList.map((product, index) => (
+    <Form.Item label={product.name} key={index}>
+      <InputNumber
+        min={0}
+        defaultValue={localStorage.getItem(product.pid)}
+        onChange={(value) => {
+          localStorage.setItem(product.pid, value);
+          setupdateTotalCost(!updateTotalCost);
+        }}
+      />
+      @${product.price}
+      <MinusCircleOutlined
+        onClick={() => {
+          removeShoppingList(product);
+          //localStorage.removeItem(product.pid);
+          //setDeleteShoppingCart(!deleteShoppingCart);
+        }}
+      />
+    </Form.Item>
+  ));
 
-    const shoppingCartElements = getShoppingCartList.map((product, index) =>
-            <Form.Item 
-                label={product.name}
-                key={index}
-            >
-                    <InputNumber 
-                        min={0} 
-                        defaultValue={localStorage.getItem(product.pid)} 
-                        onChange={(value)=>{
-                            localStorage.setItem(product.pid, value);
-                            setupdateTotalCost(!updateTotalCost);
-                        }} 
-                    />
-                        @${product.price}
-            </Form.Item>
-    );
+  const totalCost = getShoppingCartList.reduce(
+    (total, product) =>
+      total + product.price * localStorage.getItem(product.pid),
+    0
+  );
 
-    const totalCost = getShoppingCartList.reduce((total, product) => 
-        total + product.price * localStorage.getItem(product.pid), 
-        0
-    );
+  return (
+    <div id="foo">
+      <p>Shopping List Total: ${totalCost}</p>
 
-    return (
-        <div id="foo">
-            <p>Shopping List Total: ${totalCost}</p>
-
-            {shoppingCartElements}
-            <button onClick={onFinish}>Checkout</button>
-            
-        </div>
-    );
-}
+      {shoppingCartElements}
+      <button onClick={onFinish}>Checkout</button>
+    </div>
+  );
+};
 
 export default ShoppingCart;
--- src/contexts/ShoppingCartContext.js
+++ src/contexts/ShoppingCartContext.js
@@ -1,31 +1,44 @@
-import React, {useContext, useState} from 'react';
+import React, { useContext, useState } from "react";
 
 const ShoppingCartContext = React.createContext();
 const ShoppingCartUpdateContext = React.createContext();
+const ShoppingCartDeleteContext = React.createContext();
 
-export function useShoppingCart(){
-    return useContext(ShoppingCartContext);
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext);
 }
 
 export function useShoppingCartUpdate() {
-    return useContext(ShoppingCartUpdateContext);
+  return useContext(ShoppingCartUpdateContext);
 }
 
-export function ShoppingCartProvider({children}) {
-    const [shoppingCart, setShoppingCart] = useState(false);
+export function useShoppingCartDelete() {
+  return useContext(ShoppingCartDeleteContext);
+}
 
-    const addShoppingList = (newProduct) => {
-        if (localStorage.getItem(newProduct.pid) == null){
-            localStorage.setItem(newProduct.pid, 1);
-            setShoppingCart(updateShoppingCart => !updateShoppingCart);
-        }      
+export function ShoppingCartProvider({ children }) {
+  const [shoppingCart, setShoppingCart] = useState(false);
+  const [deleteShoppingCart, setDeleteShoppingCart] = useState(false);
+
+  const addShoppingList = (newProduct) => {
+    if (localStorage.getItem(newProduct.pid) == null) {
+      localStorage.setItem(newProduct.pid, 1);
+      setShoppingCart((updateShoppingCart) => !updateShoppingCart);
     }
+  };
 
-    return (
-        <ShoppingCartContext.Provider value={shoppingCart}>
-            <ShoppingCartUpdateContext.Provider value={addShoppingList}>
-                {children}
-            </ShoppingCartUpdateContext.Provider>
-        </ShoppingCartContext.Provider>
-    );
+  const removeShoppingList = (oldProduct) => {
+    localStorage.removeItem(oldProduct.pid);
+    setDeleteShoppingCart((deleteShoppingCart) => !deleteShoppingCart);
+  };
+
+  return (
+    <ShoppingCartContext.Provider value={shoppingCart}>
+      <ShoppingCartUpdateContext.Provider value={addShoppingList}>
+        <ShoppingCartDeleteContext.Provider value={removeShoppingList}>
+          {children}
+        </ShoppingCartDeleteContext.Provider>
+      </ShoppingCartUpdateContext.Provider>
+    </ShoppingCartContext.Provider>
+  );
 }
