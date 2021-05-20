7a4bdfd60fb4deeb7d3ee6097c103dfc79a31e55
sinyiwsy <1155110677@link.cuhk.edu.hk>
2021-05-21 05:06:22 +0800

    Add checkout button

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -2,9 +2,18 @@ import ShoppingCart from "../components/ShoppingCart";
 import PaymentForm from "./PaymentForm";
 import { useUser } from "../contexts/UserContext";
 import { Button, Row, Col } from "antd";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const CheckoutPage = (props) => {
+  const history = useHistory();
   const user = useUser();
+  useEffect(() => {
+    if (user === "Guest") {
+      alert("Please Login!");
+      history.push("/login");
+    }
+  });
 
   return (
     <>
