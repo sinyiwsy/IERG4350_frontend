4af032c2f58cb334762aea7cd336df7ae5923f43
thomaslty <thomaslty16@gmail.com>
2021-05-20 22:25:53 +0800

    use session id as order number

--- src/pages/PaymentResultPage/PaymentSuccessPage.js
+++ src/pages/PaymentResultPage/PaymentSuccessPage.js
@@ -1,23 +1,24 @@
-import { Result, Button } from 'antd';
+import { Result, Button } from "antd";
 import { useHistory } from "react-router-dom";
 
-
 const PaymentSuccessPage = () => {
-    const history = useHistory();
+  const history = useHistory();
+  const urlParams = new URLSearchParams(window.location.search);
+  const sessionId = urlParams.get("session_id");
 
-    return (
-        <Result
-            status="success"
-            title="Successfully Purchased 4350!"
-            subTitle="Order number: 123456"
-            extra={[
-                <Button type="primary" key="console" onClick={() => history.push('/')}>
-                    Go Home Page
-                </Button>,
-                <Button key="buy">Buy More</Button>,
-            ]}
-        />
-    );
-}   
+  return (
+    <Result
+      status="success"
+      title="Successfully Purchased 4350!"
+      subTitle={"Order number: " + sessionId.replace(/cs_test_/, "")}
+      extra={[
+        <Button type="primary" key="console" onClick={() => history.push("/")}>
+          Go Home Page
+        </Button>,
+        <Button key="buy">Buy More</Button>,
+      ]}
+    />
+  );
+};
 
 export default PaymentSuccessPage;
